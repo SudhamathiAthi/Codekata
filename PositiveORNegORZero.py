992260ee5d26e35967b41d5d44f6bd39b5498c00
SudhamathiAthi <sudhamathi.a@gmail.com>
2017-06-21 12:28:02 +0530

    Create PositiveORNegORZero.py

---
+++ PositiveORNegORZero.py
@@ -0,0 +1,7 @@
+num=input("Enter the number to test:")
+    if(num>0):
+        print +str(num),"is a positive number"
+    elif(num<0):
+        print +str(num),"is a negative number"
+    else:
+        print "It is zero"
