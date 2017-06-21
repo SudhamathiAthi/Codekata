5c7db44577252d79eab3e6e3fc986ad2889498f3
SudhamathiAthi <sudhamathi.a@gmail.com>
2017-06-21 11:55:43 +0530

    Create VowelOrConsonant.py

---
+++ VowelOrConsonant.py
@@ -0,0 +1,10 @@
+    String1=input("Enter String:")
+    for i in range(0,1):
+        if (String1=='a' or String1=='A' or
+        String1=='e' or String1=='E' or
+        String1=='i' or String1=='I' or
+        String1=='o' or String1=='O'
+        or String1=='u' or String1=='U'):
+            print "Vowel"
+        else:
+            print "Consonant"
