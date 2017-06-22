a2ec6526169208c9272899ac1acf54b5be50f9b4
SudhamathiAthi <sudhamathi.a@gmail.com>
2017-06-22 09:27:09 +0530

    Create sumofN.py

---
+++ sumofN.py
@@ -0,0 +1,7 @@
+limit=int(input("Enter the limit no:"))
+    sum=0
+    for i in range(0,limit):
+        nums=input("Enter the numbers:")
+    for i in range(1,nums+1):
+        sum=sum+i
+    print sum
